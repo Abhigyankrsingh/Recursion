815b5441a59f194e4d0fb850676d523cda4ae4e8
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-04 20:40:01 +0530

    Code_Updated

---
+++ rec3.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void  prnt(int i,int n){
+   
+   if(i>n) return;
+
+   cout<<"name"<<endl;
+    
+
+     prnt(i+1,n);
+   
+
+}
+
+
+
+
+
+int main(){
+      int n;
+    cin >> n;
+   prnt(1,n);
+   return 0;
+}
